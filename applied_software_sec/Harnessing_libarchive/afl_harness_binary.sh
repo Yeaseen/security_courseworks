d99fe6c576f6c5192c4ab6677a8597bc4e57d6ba
Yeaseen <yeaseen.arafat96@gmail.com>
2024-02-28 19:58:56 -0700

    Harnessing_done

--- applied_software_sec/Harnessing_libarchive/afl_harness_binary.sh
+++ applied_software_sec/Harnessing_libarchive/afl_harness_binary.sh
@@ -1,23 +1,13 @@
 #Fuzzing System's libarchive binary shared file in QEMU mode
 #for this you need to have clean libarchive installed in a regular path
 #in case, you somehow messed up with the system's libarchive
-sudo apt-get install --reinstall libarchive-dev
+
+#sudo apt-get install --reinstall libarchive-dev
+
+#Verify that the new version of `libarchive` is correctly installed and recognized
+#ldconfig -p | grep libarchive
 
 gcc -o harness_fuzzer test_libarchive.c -larchive
+#AFL_INST_LIBS=1 AFL_DEBUG=1 /mnt/bigdata/YEASEEN/PG/AFLPlusPlus/afl-fuzz -Q -i input/ -o findings/ -- ./harness_fuzzer @@
 AFL_INST_LIBS=1 /mnt/bigdata/YEASEEN/PG/AFLPlusPlus/afl-fuzz -Q -i input/ -o findings/ -- ./harness_fuzzer @@
-
-
-#If you instrumented the system's libarchive shared library (*.so file) in QEMU mode 
-#using AFL (American Fuzzy Lop) yesterday, then the libarchive on your system is currently 
-#AFL-instrumented. This means that the library has been modified to include AFL's instrumentation 
-#code, which is used for fuzz testing by monitoring the code paths that are executed during 
-#runtime. The AFL instrumentation is designed to help identify inputs that can trigger bugs 
-#or unexpected behavior in the program being fuzzed.
-
-#However, this instrumentation can lead to compatibility issues with applications or 
-#build processes that are not part of the fuzz testing setup, as you've experienced. 
-#The instrumented library expects certain AFL-specific symbols and runtime support, 
-#which are not present when running applications normally or using build tools like CMake 
-#in a standard environment.
-
-#so reinstall the system's shared library file of libarchive for further use of make or something
+#AFL_INST_LIBS=1 /mnt/bigdata/YEASEEN/PG/AFLPlusPlus/afl-fuzz -Q -D -i input/ -o findings/ -- ./harness_fuzzer @@
--- applied_software_sec/Harnessing_libarchive/aflpp_libFuzz.sh
+++ applied_software_sec/Harnessing_libarchive/aflpp_libFuzz.sh
@@ -9,8 +9,14 @@
 
 #make all
 
-
+export LD_LIBRARY_PATH=/mnt/bigdata/YEASEEN/PG/Harnessing_libarchive/libarchive/libarchive_install/lib/:$LD_LIBRARY_PATH
 
 /mnt/bigdata/YEASEEN/PG/AFLPlusPlus/afl-clang-fast++ -o fuzzer_harness fuzzer_harness.cc /mnt/bigdata/YEASEEN/PG/AFLPlusPlus/libAFLDriver.a -L libarchive/libarchive_install/lib/ -l archive -I libarchive/libarchive_install/lib/ -fsanitize=address
+
+#the following command is for checking which dll the binary is using
+#ldd fuzzer_harness
+#the following command is for checking whether the library file actually instrumented by afl
+#objdump -D /mnt/bigdata/YEASEEN/PG/Harnessing_libarchive/libarchive/libarchive_install/lib/libarchive.so | grep afl
+
 
 /mnt/bigdata/YEASEEN/PG/AFLPlusPlus/afl-fuzz -i input -o output_dir -- ./fuzzer_harness @@
